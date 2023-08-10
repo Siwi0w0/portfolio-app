8534c5b05b63b883e921b16e538b4bc6047bb955
Siwi0w0 <yufei.sun64@gmail.com>
2023-08-10 13:38:25 +0100

    Update Home Page

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Data = () => {
+    return (
+        <div className="home_data">
+            <h1 className="home_title">Yufei Sun
+            </h1>
+            <h3 className="home_subtitle">Front End Developer/Visual Designer</h3>
+            <p className="home_description">I design and develop services for customers of all sizes, specializing in creating stylish, modern websites.</p>
+
+            <a href="#contact" className="button button--flex">
+                Say Hello!
+            </a>
+        </div>
+    )
+}
+
+export default Data;
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './home.css'
+import Social from './Social';
+import Data from './Data';
+
+const Home = () => {
+    return (
+        <section className="home section" id="home">
+            <div className="home_container container grid">
+                <div className="home_content grid">
+                    <Social />
+                    <div className="home_img_bg">
+                        <div className="home_img">
+                        </div>
+                    </div>
+                    
+
+                    <Data />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Home;
